a490cdcf625aeb708017bd1bbe6417b647ec45e2
Dave Paola <dpaola2@gmail.com>
2026-01-19 08:33:47 -0500

    Initial Commit

---
+++ booktracker-ios/booktracker_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  booktracker_iosApp.swift
+//  booktracker-ios
+//
+//  Created by Dave Paola on 1/19/26.
+//
+
+import SwiftUI
+
+@main
+struct booktracker_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
